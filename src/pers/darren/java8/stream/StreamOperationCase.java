545f78bbc5bdc03bd98c56024b0efa0f491b75f8
Darren Luo <darrenluo1993@163.com>
2022-03-03 16:35:48 +0800

    Java8 stream学习案例

--- src/pers/darren/java8/stream/StreamOperationCase.java
+++ src/pers/darren/java8/stream/StreamOperationCase.java
@@ -18,9 +18,12 @@ import java.util.stream.Stream;
  * 3. stream具有延迟执行特性，只有调用终端操作时，中间操作才会执行。
  * <p>
  * Stream可以通过集合数组创建。
+ * <p>
+ * 参考文章：https://blog.csdn.net/mu_wind/article/details/109516995
  */
 public class StreamOperationCase {
 
+    private static final Integer point = 5000;
     private static final List<Person> personList = new ArrayList<>();
 
     static {
@@ -30,6 +33,7 @@ public class StreamOperationCase {
         personList.add(new Person("Anni", 8200, 24, "female", "New York"));
         personList.add(new Person("Owen", 9500, 25, "male", "New York"));
         personList.add(new Person("Alisa", 7900, 26, "female", "New York"));
+        personList.add(new Person("Darren", 9500, 24, "male", "Chang Sha"));
     }
 
     public static void main(String[] args) {
@@ -38,7 +42,14 @@ public class StreamOperationCase {
         // filter();
         // maxMinCount();
         // mapFlatMap();
-        reduce();
+        // reduce();
+        // toListToSetToMap();
+        // countAveraging();
+        // partitioningByGroupingBy();
+        // joining();
+        // reducing();
+        // sorted();
+        concatDistinctLimitSkip();
     }
 
     /**
@@ -60,7 +71,7 @@ public class StreamOperationCase {
         stream.forEach(System.out::println);
         System.out.println("---------------------------------------------");
         // 通过parallel()将顺序流转换成并行流
-        Optional<String> findFirst = list.stream().parallel().filter(x -> "a".equals(x)).findFirst();
+        Optional<String> findFirst = list.stream().parallel().filter("a"::equals).findFirst();
         findFirst.ifPresent(System.out::println);
         System.out.println("---------------------------------------------");
         // 创建一个并行流
@@ -146,11 +157,17 @@ public class StreamOperationCase {
         System.out.println("最长的字符串：" + maxString.get());
         System.out.println("---------------------------------------------");
         // 案例二：获取Integer集合中的最大值。
-        List<Integer> integerList = Arrays.asList(6, 7, 3, 5, 9, 11, 6, 4, 0);
+        List<Integer> integerList = Arrays.asList(6, 7, 3, 5, 9, 11, 6, 4, 2);
         Optional<Integer> maxInteger = integerList.stream().max(Integer::compareTo);
-        System.out.println("自然排序最大值：" + maxInteger.get());
+        System.out.println("取自然排序最大值方式1：" + maxInteger.get());
+        maxInteger = integerList.stream().max(Comparator.naturalOrder());
+        System.out.println("取自然排序最大值方式2：" + maxInteger.get());
+        maxInteger = integerList.stream().max((i1, i2) -> i1.compareTo(i2));
+        System.out.println("自定义排序最大值：" + maxInteger.get());
+        maxInteger = integerList.stream().max((i1, i2) -> i2.compareTo(i1));
+        System.out.println("自定义排序最大值（实际是最小值）：" + maxInteger.get());
         maxInteger = integerList.stream().max(Comparator.reverseOrder());
-        System.out.println("自定义排序的最大值：" + maxInteger.get());
+        System.out.println("反自然排序最大值（实际是最小值）：" + maxInteger.get());
         System.out.println("---------------------------------------------");
         // 案例三：获取员工工资最高的人。
         Optional<Person> maxPerson = personList.stream().max(Comparator.comparingInt(Person::getSalary));
@@ -280,5 +297,210 @@ public class StreamOperationCase {
         // 求最高工资方式5
         maxVal = personList.stream().reduce(0, (m, person) -> m > person.getSalary() ? m : person.getSalary(), (m1, m2) -> m1 > m2 ? m1 : m2);
         System.out.println("求最高工资方式5：" + maxVal);
+    }
+
+    // 收集(collect)
+    // collect，收集，可以说是内容最繁多、功能最丰富的部分了。从字面上去理解，就是把一个流收集起来，最终可以是收集成一个值也可以收集成一个新的集合。
+
+    /**
+     * 归集(toList/toSet/toMap)
+     * 因为流不存储数据，那么在流中的数据完成处理后，需要将流中的数据重新归集到新的集合里。toList、toSet和toMap比较常用，另外还有toCollection、toConcurrentMap等复杂一些的用法。
+     *
+     * @CreatedBy Darren Luo
+     * @CreatedTime 3/2/22 11:40 AM
+     */
+    private static void toListToSetToMap() {
+        List<Integer> list = Arrays.asList(1, 6, 3, 4, 6, 7, 9, 6, 20);
+        List<Integer> evenList = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        System.out.println("toList：" + evenList);
+        Set<Integer> evenSet = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toSet());
+        System.out.println("toSet：" + evenSet);
+        // 筛选出工资高于8000的人
+        Map<String, Person> personMap = personList.stream().filter(person -> person.getSalary() > 8000).collect(Collectors.toMap(Person::getName, person -> person));
+        System.out.println("toMap，工资高于8000的人：" + personMap);
+        // 筛选出工资高于8000的人名和具体工资
+        Map<String, Integer> salaryMap = personList.stream().filter(person -> person.getSalary() > 8000).collect(Collectors.toMap(Person::getName, Person::getSalary));
+        System.out.println("toMap，工资高于8000的人名和具体工资：" + salaryMap);
+    }
+
+    /**
+     * 统计(count/averaging)
+     * Collectors提供了一系列用于数据统计的静态方法：
+     * 计数：count
+     * 平均值：averagingInt、averagingLong、averagingDouble
+     * 最值：maxBy、minBy
+     * 求和：summingInt、summingLong、summingDouble
+     * 统计以上所有：summarizingInt、summarizingLong、summarizingDouble
+     *
+     * @CreatedBy Darren Luo
+     * @CreatedTime 3/2/22 3:23 PM
+     */
+    private static void countAveraging() {
+        // 案例：统计员工人数、平均工资、工资总额、最高工资。
+        // 求总数
+        Long count = personList.stream().filter(person -> person.getSalary() > 8000).collect(Collectors.counting());
+        System.out.println("求总数方式1：" + count);
+        count = personList.stream().filter(person -> person.getSalary() > 8000).count();
+        System.out.println("求总数方式2：" + count);
+        System.out.println("---------------------------------------------");
+        // 求平均工资
+        Double average = personList.stream().collect(Collectors.averagingDouble(Person::getSalary));
+        System.out.println("求平均工资方式：" + average);
+        System.out.println("---------------------------------------------");
+        // 求最高工资
+        Optional<Integer> max = personList.stream().map(Person::getSalary).collect(Collectors.maxBy(Integer::compareTo));
+        System.out.println("求最高工资方式1：" + max.get());
+        max = personList.stream().map(Person::getSalary).max(Integer::compareTo);
+        System.out.println("求最高工资方式2：" + max.get());
+        System.out.println("---------------------------------------------");
+        // 求工资之和
+        int sum = personList.stream().collect(Collectors.summingInt(Person::getSalary));
+        System.out.println("求工资之和方式1：" + sum);
+        sum = personList.stream().mapToInt(Person::getSalary).sum();
+        System.out.println("求工资之和方式2：" + sum);
+        System.out.println("---------------------------------------------");
+        // 一次性统计所有信息
+        DoubleSummaryStatistics statistics = personList.stream().collect(Collectors.summarizingDouble(Person::getSalary));
+        System.out.println("员工工资所有统计信息：" + statistics);
+    }
+
+    /**
+     * 分组(partitioningBy/groupingBy)
+     * 分区：将stream按条件分为两个Map，比如员工按薪资是否高于8000分为两部分。
+     * 分组：将集合分为多个Map，比如员工按性别分组。有单级分组和多级分组。
+     *
+     * @CreatedBy Darren Luo
+     * @CreatedTime 3/2/22 4:05 PM
+     */
+    private static void partitioningByGroupingBy() {
+        // 案例：将员工按薪资是否高于8000分为两部分；将员工按性别和地区分组
+        // 将员工按薪资是否高于8000分组
+        Map<Boolean, List<Person>> part = personList.stream().collect(Collectors.partitioningBy(person -> person.getSalary() > 8000));
+        System.out.println("员工按薪资是否高于8000分组：" + part);
+        // 将员工按性别分组
+        Map<String, List<Person>> group = personList.stream().collect(Collectors.groupingBy(Person::getSex));
+        System.out.println("员工按性别分组：" + group);
+        // 将员工先按性别分组，再按地区分组
+        Map<String, Map<String, List<Person>>> groupGroup = personList.stream().collect(Collectors.groupingBy(Person::getSex, Collectors.groupingBy(Person::getArea)));
+        System.out.println("员工先按性别分组，再按地区分组：" + groupGroup);
+    }
+
+    /**
+     * 接合(joining)
+     * joining可以将stream中的元素用特定的连接符（没有的话，则直接连接）连接成一个字符串。
+     *
+     * @CreatedBy Darren Luo
+     * @CreatedTime 3/2/22 5:01 PM
+     */
+    private static void joining() {
+        String names = personList.stream().map(Person::getName).collect(Collectors.joining(",", "<", ">"));
+        System.out.println("所有员工的姓名：" + names);
+        String string = Stream.of("A", "B", "C", "D").collect(Collectors.joining("-"));
+        System.out.println("拼接后的字符串：" + string);
+        string = Stream.of(1, 2, 3, 4, 5).map(String::valueOf).collect(Collectors.joining("--"));
+        System.out.println("数字拼接后的字符串：" + string);
+    }
+
+    /**
+     * 归约(reducing)
+     * Collectors类提供的reducing方法，相比于stream本身的reduce方法，增加了对自定义归约的支持。
+     *
+     * @CreatedBy Darren Luo
+     * @CreatedTime 3/2/22 5:30 PM
+     */
+    private static void reducing() {
+        // 员工工资总和
+        int sumVal = personList.stream().mapToInt(Person::getSalary).sum();
+        System.out.println("员工工资总和：" + sumVal);
+        // 求员工工资减去税收起征点后的总和
+        Optional<Integer> sum = personList.stream().map(person -> person.getSalary() - point).collect(Collectors.reducing(Integer::sum));
+        System.out.println("求员工工资减去税收起征点后的总和方式1：" + sum.get());
+        sum = personList.stream().map(person -> person.getSalary() - point).reduce(Integer::sum);
+        System.out.println("求员工工资减去税收起征点后的总和方式2：" + sum.get());
+        sumVal = personList.stream().mapToInt(person -> person.getSalary() - point).sum();
+        System.out.println("求员工工资减去税收起征点后的总和方式3：" + sumVal);
+        sumVal = personList.stream().collect(Collectors.reducing(0, Person::getSalary, (x, y) -> x + y - point));
+        System.out.println("求员工工资减去税收起征点后的总和方式4：" + sumVal);
+    }
+
+    /**
+     * 排序(sorted)
+     * sorted，中间操作。有两种排序：
+     * sorted()：自然排序，流中元素需实现Comparable接口
+     * sorted(Comparator com)：Comparator排序器自定义排序
+     *
+     * @CreatedBy Darren Luo
+     * @CreatedTime 3/3/22 11:31 AM
+     */
+    private static void sorted() {
+        List<Integer> numList = Arrays.asList(4, 6, 7, 0, 2, 3, 1, 8, 9, 5);
+        List<Integer> sortedNumList = numList.stream().sorted(Integer::compareTo).collect(Collectors.toList());
+        System.out.println("升序排序方式1：" + sortedNumList);
+        sortedNumList = numList.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+        System.out.println("升序排序方式2：" + sortedNumList);
+        sortedNumList = numList.stream().sorted(Comparator.comparing(num -> num)).collect(Collectors.toList());
+        System.out.println("升序排序方式3：" + sortedNumList);
+        System.out.println("---------------------------------------------");
+        sortedNumList = numList.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        System.out.println("降序排序方式1：" + sortedNumList);
+        sortedNumList = numList.stream().sorted(Comparator.comparing(num -> (int) num).reversed()).collect(Collectors.toList());
+        System.out.println("降序排序方式2：" + sortedNumList);
+        System.out.println("---------------------------------------------");
+        // 案例：将员工按工资由高到低（工资一样则按年龄由大到小）排序
+        // 按工资升序排序（自然排序）
+        List<Person> sortedPersonList = personList.stream().sorted(Comparator.comparing(Person::getSalary)).collect(Collectors.toList());
+        System.out.println("按工资升序排序方式1：" + sortedPersonList);
+        sortedPersonList = personList.stream().sorted(Comparator.comparing(Person::getSalary, Comparator.naturalOrder())).collect(Collectors.toList());
+        System.out.println("按工资升序排序方式2：" + sortedPersonList);
+        System.out.println("---------------------------------------------");
+        // 按工资降序排序
+        sortedPersonList = personList.stream().sorted(Comparator.comparing(Person::getSalary).reversed()).collect(Collectors.toList());
+        System.out.println("按工资降序排序方式1：" + sortedPersonList);
+        sortedPersonList = personList.stream().sorted(Comparator.comparing(Person::getSalary, Comparator.reverseOrder())).collect(Collectors.toList());
+        System.out.println("按工资降序排序方式2：" + sortedPersonList);
+        System.out.println("---------------------------------------------");
+        // 先按工资再按年龄升序排序
+        sortedPersonList = personList.stream().sorted(Comparator.comparing(Person::getSalary).thenComparing(Person::getAge)).collect(Collectors.toList());
+        System.out.println("先按工资再按年龄升序排序：" + sortedPersonList);
+        System.out.println("---------------------------------------------");
+        // 先按年龄再按工资升序排序
+        sortedPersonList = personList.stream().sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getSalary)).collect(Collectors.toList());
+        System.out.println("先按年龄再按工资升序排序：" + sortedPersonList);
+        System.out.println("---------------------------------------------");
+        // 先按工资再按年龄自定义排序（降序）
+        sortedPersonList = personList.stream().sorted((p1, p2) -> {
+            if (p1.getSalary() == p2.getSalary()) {
+                return p2.getAge() - p1.getAge();
+            }
+            return p2.getSalary() - p1.getSalary();
+        }).collect(Collectors.toList());
+        System.out.println("先按工资再按年龄自定义排序（降序）：" + sortedPersonList);
+    }
+
+    /**
+     * 提取/组合(concat/distinct/limit/skip)
+     * 流也可以进行合并、去重、限制、跳过等操作。
+     *
+     * @CreatedBy Darren Luo
+     * @CreatedTime 3/3/22 3:42 PM
+     */
+    private static void concatDistinctLimitSkip() {
+        Stream<String> stream1 = Stream.of("a", "b", "c", "d");
+        Stream<String> stream2 = Stream.of("d", "e", "f", "g");
+        // concat合并两个stream，再distinct去重
+        List<String> stringList = Stream.concat(stream1, stream2).distinct().collect(Collectors.toList());
+        System.out.println("concat合并流，再distinct去重：" + stringList);
+        // limit限制从流中获得前n个数据
+        List<Integer> integerList = Stream.iterate(1, x -> x + 2).limit(10).collect(Collectors.toList());
+        System.out.println("limit限制从流中获得前n个数据：" + integerList);
+        // 先skip跳过前n个数据，再limit获得前n个数据
+        integerList = Stream.iterate(1, x -> x + 2).skip(1).limit(5).collect(Collectors.toList());
+        System.out.println("先skip跳过前n个数据，再limit获得前n个数据：" + integerList);
+        // 先limit获得前n个数据，再skip跳过前n个数据
+        integerList = Stream.iterate(1, x -> x + 2).limit(6).skip(1).collect(Collectors.toList());
+        System.out.println("先limit获得前n个数据，再skip跳过前n个数据：" + integerList);
+        // skip跳过第1个员工，再limit取3个员工
+        List<Person> newPersonList = personList.stream().skip(1).limit(3).collect(Collectors.toList());
+        System.out.println("skip跳过第1个员工，再limit取3个员工：" + newPersonList);
     }
 }
