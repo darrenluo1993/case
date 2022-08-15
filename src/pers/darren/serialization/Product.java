cf22e759bf652f4ab39d033be731906c4be0054f
Darren Luo <darrenluo1993@163.com>
2022-08-15 10:13:33 +0800

    JAVA序列化与反序列化实践案例

--- src/pers/darren/serialization/Product.java
+++ src/pers/darren/serialization/Product.java
@@ -1,5 +1,6 @@
 package pers.darren.serialization;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
@@ -11,126 +12,152 @@ import java.util.Date;
  * @CreatedTime Aug 7, 2019 5:38:02 PM
  */
 public class Product implements Serializable {
+    /**
+     * <pre>
+     * 为什么要显式指定serialVersionUID的值?
+     * 1、如果不显式指定serialVersionUID, JVM在序列化时会根据属性自动生成一个serialVersionUID, 然后与属性一起序列化, 再进行持久化或网络传输. 在反序列化时, JVM会再根据属性自动生成一个新版serialVersionUID, 然后将这个新版serialVersionUID与序列化时生成的旧版serialVersionUID进行比较, 如果相同则反序列化成功, 否则报错.
+     * 2、如果显式指定了serialVersionUID, JVM在序列化和反序列化时仍然都会生成一个serialVersionUID, 但值为我们显式指定的值, 这样在反序列化时新旧版本的serialVersionUID就一致了.
+     * 3、在实际开发中, 不显式指定serialVersionUID的情况会导致什么问题? 如果我们的类写完后不再修改, 那当然不会有问题, 但这在实际开发中是不可能的, 我们的类会不断迭代, 一旦类被修改了, 那旧对象反序列化就会报错. 所以在实际开发中, 我们都会显式指定一个serialVersionUID, 值是多少无所谓, 只要不变就行.
+     * </pre>
+     */
+    @Serial
+    private static final long serialVersionUID = 7241915439741270646L;
+    /**
+     * <pre>
+     * static修饰的字段，不会被序列化
+     *
+     * 1、static属性为什么不会被序列化?
+     * 因为序列化是针对对象而言的, 而static属性优先于对象存在, 随着类的加载而加载, 所以不会被序列化.
+     * 2、serialVersionUID也被static修饰, 为什么serialVersionUID会被序列化?
+     * 其实serialVersionUID属性并没有被序列化, JVM在序列化对象时会自动生成一个serialVersionUID, 然后将我们显式指定的serialVersionUID属性值赋给自动生成的serialVersionUID.
+     * </pre>
+     */
+    public static final String defaultUser = "Darren Luo";
 
-	private static final long serialVersionUID = 7241915439741270646L;
+    private String name;
 
-	public static final String defaultUser = "Darren Luo";
+    private String category;
 
-	private String name;
+    private String supplier;
 
-	private String category;
+    private int quantity;
 
-	private String supplier;
+    private BigDecimal price;
 
-	private int quantity;
+    private boolean featured;
 
-	private BigDecimal price;
+    private String description;
+    /**
+     * 创建人
+     * transient修饰的字段，不会被序列化
+     */
+    private transient String createdBy;
+    /**
+     * 创建时间
+     * transient修饰的字段，不会被序列化
+     */
+    private transient Date createdTime;
+    /**
+     * 修改人
+     * transient修饰的字段，不会被序列化
+     */
+    private transient String modifiedBy;
+    /**
+     * 修改时间
+     * transient修饰的字段，不会被序列化
+     */
+    private transient Date modifiedTime;
 
-	private boolean featured;
+    public String getName() {
+        return name;
+    }
 
-	private String description;
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	private transient String createdBy;
+    public String getCategory() {
+        return category;
+    }
 
-	private transient Date createdTime;
+    public void setCategory(String category) {
+        this.category = category;
+    }
 
-	private transient String modifiedBy;
+    public String getSupplier() {
+        return supplier;
+    }
 
-	private transient Date modifiedTime;
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
-	public String getCategory() {
-		return category;
-	}
+    public BigDecimal getPrice() {
+        return price;
+    }
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 
-	public String getSupplier() {
-		return supplier;
-	}
+    public boolean isFeatured() {
+        return featured;
+    }
 
-	public void setSupplier(String supplier) {
-		this.supplier = supplier;
-	}
+    public void setFeatured(boolean featured) {
+        this.featured = featured;
+    }
 
-	public int getQuantity() {
-		return quantity;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public BigDecimal getPrice() {
-		return price;
-	}
+    public String getCreatedBy() {
+        return createdBy;
+    }
 
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
 
-	public boolean isFeatured() {
-		return featured;
-	}
+    public Date getCreatedTime() {
+        return createdTime;
+    }
 
-	public void setFeatured(boolean featured) {
-		this.featured = featured;
-	}
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
 
-	public String getCreatedBy() {
-		return createdBy;
-	}
+    public Date getModifiedTime() {
+        return modifiedTime;
+    }
 
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
+    public void setModifiedTime(Date modifiedTime) {
+        this.modifiedTime = modifiedTime;
+    }
 
-	public Date getCreatedTime() {
-		return createdTime;
-	}
-
-	public void setCreatedTime(Date createdTime) {
-		this.createdTime = createdTime;
-	}
-
-	public String getModifiedBy() {
-		return modifiedBy;
-	}
-
-	public void setModifiedBy(String modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
-
-	public Date getModifiedTime() {
-		return modifiedTime;
-	}
-
-	public void setModifiedTime(Date modifiedTime) {
-		this.modifiedTime = modifiedTime;
-	}
-
-	@Override
-	public String toString() {
-		return "Product [name=" + name + ", category=" + category + ", supplier=" + supplier + ", quantity=" + quantity
-				+ ", price=" + price + ", featured=" + featured + ", description=" + description + ", createdBy="
-				+ createdBy + ", createdTime=" + createdTime + ", modifiedBy=" + modifiedBy + ", modifiedTime="
-				+ modifiedTime + "]";
-	}
+    @Override
+    public String toString() {
+        return "Product [name=" + name + ", category=" + category + ", supplier=" + supplier + ", quantity=" + quantity + ", price=" + price + ", featured=" + featured + ", description=" + description + ", createdBy=" + createdBy + ", createdTime=" + createdTime + ", modifiedBy=" + modifiedBy + ", modifiedTime=" + modifiedTime + "]";
+    }
 }
--- src/pers/darren/serialization/Serializable.java
+++ src/pers/darren/serialization/Serializable.java
@@ -9,62 +9,62 @@ import java.util.Date;
 
 public class Serializable {
 
-	public static void main(String[] args) throws Exception {
-		// serializeString();
-		serializeProduct();
-		// deserializeProduct();
-	}
+    public static void main(String[] args) throws Exception {
+        serializeString();
+        serializeProduct();
+        deserializeProduct();
+    }
 
-	private static void serializeString() throws Exception {
-		FileOutputStream fileOutStream = new FileOutputStream("/home/darren/Desktop/Temp Files/String.txt");
-		ObjectOutputStream outStream = new ObjectOutputStream(fileOutStream);
+    private static void serializeString() throws Exception {
+        FileOutputStream fileOutStream = new FileOutputStream("/home/darren/Temporary/serialization/String.txt");
+        ObjectOutputStream outStream = new ObjectOutputStream(fileOutStream);
 
-		String string = "这是一个需要被序列化的字符串！";
-		outStream.writeObject(string);
-		outStream.flush();
-		outStream.close();
+        String string = "这是一个需要被序列化的字符串！";
+        outStream.writeObject(string);
+        outStream.flush();
+        outStream.close();
 
-		FileInputStream fileInStream = new FileInputStream("/home/darren/Desktop/Temp Files/String.txt");
-		ObjectInputStream inStream = new ObjectInputStream(fileInStream);
-		string = (String) inStream.readObject();
-		inStream.close();
-		System.out.println(string);
-	}
+        FileInputStream fileInStream = new FileInputStream("/home/darren/Temporary/serialization/String.txt");
+        ObjectInputStream inStream = new ObjectInputStream(fileInStream);
+        string = (String) inStream.readObject();
+        inStream.close();
+        System.out.println(string);
+    }
 
-	private static void serializeProduct() throws Exception {
-		FileOutputStream fileOutStream = new FileOutputStream("/home/darren/Desktop/Temp Files/Product.txt");
-		ObjectOutputStream outStream = new ObjectOutputStream(fileOutStream);
+    private static void serializeProduct() throws Exception {
+        FileOutputStream fileOutStream = new FileOutputStream("/home/darren/Temporary/serialization/Product.txt");
+        ObjectOutputStream outStream = new ObjectOutputStream(fileOutStream);
 
-		Product product = new Product();
-		product.setName("序列化与反序列化");
-		product.setCategory("序列化");
-		product.setSupplier("供应商");
-		product.setQuantity(10000);
-		product.setPrice(new BigDecimal(1000.5));
-		product.setFeatured(true);
-		product.setDescription("这是一个用于测试序列化与反序列化的产品！");
-		product.setCreatedBy("Darren Luo");
-		product.setCreatedTime(new Date());
-		product.setModifiedBy("Darren Luo");
-		product.setModifiedTime(new Date());
-		System.out.println("Before>>>" + product);
+        Product product = new Product();
+        product.setName("序列化与反序列化");
+        product.setCategory("序列化");
+        product.setSupplier("供应商");
+        product.setQuantity(10000);
+        product.setPrice(new BigDecimal(1000.5));
+        product.setFeatured(true);
+        product.setDescription("这是一个用于测试序列化与反序列化的产品！");
+        product.setCreatedBy("Darren Luo");
+        product.setCreatedTime(new Date());
+        product.setModifiedBy("Darren Luo");
+        product.setModifiedTime(new Date());
+        System.out.println("Before>>>" + product);
 
-		outStream.writeObject(product);
-		outStream.flush();
-		outStream.close();
+        outStream.writeObject(product);
+        outStream.flush();
+        outStream.close();
 
-		FileInputStream fileInStream = new FileInputStream("/home/darren/Desktop/Temp Files/Product.txt");
-		ObjectInputStream inStream = new ObjectInputStream(fileInStream);
-		product = (Product) inStream.readObject();
-		inStream.close();
-		System.out.println("After>>>" + product);
-	}
+        FileInputStream fileInStream = new FileInputStream("/home/darren/Temporary/serialization/Product.txt");
+        ObjectInputStream inStream = new ObjectInputStream(fileInStream);
+        product = (Product) inStream.readObject();
+        inStream.close();
+        System.out.println("After>>>" + product);
+    }
 
-	private static void deserializeProduct() throws Exception {
-		FileInputStream fileInStream = new FileInputStream("/home/darren/Desktop/Temp Files/Product.txt");
-		ObjectInputStream inStream = new ObjectInputStream(fileInStream);
-		Product product = (Product) inStream.readObject();
-		inStream.close();
-		System.out.println("After>>>" + product);
-	}
+    private static void deserializeProduct() throws Exception {
+        FileInputStream fileInStream = new FileInputStream("/home/darren/Temporary/serialization/Product.txt");
+        ObjectInputStream inStream = new ObjectInputStream(fileInStream);
+        Product product = (Product) inStream.readObject();
+        inStream.close();
+        System.out.println("After>>>" + product);
+    }
 }
