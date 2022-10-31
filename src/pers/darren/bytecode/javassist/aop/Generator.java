4d0b42ebb53ab75c20a418886d44b0d78ffa0e91
Darren Luo <darrenluo1993@163.com>
2022-10-31 16:51:10 +0800

    利用Javassist给已存在的class添加字段并初始化值

--- src/pers/darren/bytecode/javassist/aop/Generator.java
+++ src/pers/darren/bytecode/javassist/aop/Generator.java
@@ -2,21 +2,41 @@ package pers.darren.bytecode.javassist.aop;
 
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtField;
 import javassist.CtMethod;
+
+import static javassist.CtField.Initializer.constant;
+import static javassist.Modifier.PUBLIC;
+import static javassist.Modifier.STATIC;
 
 class Generator {
 
     public static void main(String[] args) throws Exception {
         ClassPool pool = ClassPool.getDefault();
         CtClass ctClass = pool.getCtClass("pers.darren.bytecode.javassist.aop.Base");
+        CtField name = new CtField(pool.getCtClass("java.lang.String"), "name", ctClass);
+        name.setModifiers(PUBLIC);
+        name.setModifiers(STATIC);
+        ctClass.addField(name, constant("Darren Luo"));
+        CtField gender = new CtField(pool.getCtClass("java.lang.String"), "gender", ctClass);
+        gender.setModifiers(PUBLIC);
+        gender.setModifiers(STATIC);
+        ctClass.addField(gender, constant("Male"));
+        // CtField age = new CtField(pool.getCtClass("java.lang.Integer"), "age", ctClass);
+        // age.setModifiers(PUBLIC);
+        // age.setModifiers(STATIC);
+        // ctClass.addField(age, constant(18));
         CtMethod ctMethod = ctClass.getDeclaredMethod("process");
         ctMethod.addParameter(pool.getCtClass("java.lang.String"));
-        ctMethod.insertParameter(pool.getCtClass("java.lang.Integer"));
         ctMethod.addParameter(pool.getCtClass("java.lang.Long"));
+        ctMethod.insertParameter(pool.getCtClass("java.lang.Integer"));
         ctMethod.insertBefore("System.out.println(\"start\");");
         ctMethod.insertAfter("System.out.println($1);");
         ctMethod.insertAfter("System.out.println($2);");
         ctMethod.insertAfter("System.out.println($3);");
+        ctMethod.insertAfter("System.out.println(name);");
+        ctMethod.insertAfter("System.out.println(gender);");
+        // ctMethod.insertAfter("System.out.println(age);");
         ctMethod.insertAfter("System.out.println(\"end\");");
         ctClass.writeFile("target/classes/");
     }
